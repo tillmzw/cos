e9f3d76e1e97cb3aec2e8bc2f613ab2bed68c2e3
Till Meyer zu Westram <till@meyerzuwestram.net>
2025-06-11 17:08:49 +0200

    build drafts in container

--- hugo-container.sh
+++ hugo-container.sh
@@ -2,10 +2,12 @@
 
 test -d ./themes/terminal/.git || git submodule update
 
+open http://localhost:8080/cos
+
 podman run --rm \
   -p 8080:8080 \
   -v ${PWD}:/src \
   hugomods/hugo:exts-non-root \
-  server -p 8080
-
-open http://localhost:8080/cos
+  server \
+  -p 8080 \
+  --buildDrafts
